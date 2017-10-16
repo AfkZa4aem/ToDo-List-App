b8949c1b3b047dee80130348d3b53a20fb73f6d1
Unknown <nastypinagrably@gmail.com>
2017-10-16 11:45:47 +0300

    finish with styles

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -17,7 +17,7 @@ $('input[type="text"]').keypress(function(event){
 		// extract input value
 		var todoText = $(this).val();
 		// create new <li> and add to <ul>
-		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+		$("ul").append('<li><span><i class="fa fa-trash-o" aria-hidden="true"></i></span> ' + todoText + "</li>");
 		$(this).val("");
 	}
 });
