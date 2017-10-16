20e3e11e6bd9f94349cbc206efd669c1f98be31e
Unknown <nastypinagrably@gmail.com>
2017-10-16 11:04:02 +0300

    add creating todo functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,22 @@
 // Check off specific todos by clicking
-$("li").on("click", function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("complited");
 });
 
 // Click on X to delete todo
-$("span").on("click", function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$('input[type="text"]').keypress(function(event){
+	if(event.which === 13){
+		// extract input value
+		var todoText = $(this).val();
+		// create new <li> and add to <ul>
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+		$(this).val("");
+	}
 });
